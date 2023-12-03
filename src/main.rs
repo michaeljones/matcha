d2611d9be88f49e11d38660d3e83a9c3d97d50a7
Michael Jones <m.pricejones@gmail.com>
2023-12-03 19:50:17 +0000

    Re-write requires_update logic to remove unwraps
    And to use a direct comparison instead of the duration_since method
    which I assume is ok.
    
    Otherwise, we just and_then the results and exit early if we're not
    getting access to the 'modified' times that we need to make the
    decision. We conservatively require an update if we fail to access the
    modified times.

--- src/main.rs
+++ src/main.rs
@@ -60,18 +60,23 @@ fn color_choice() -> ColorChoice {
 }
 
 fn requires_update(matcha_path: &std::path::Path) -> bool {
-    let matcha_meta = std::fs::metadata(matcha_path).unwrap();
-    let matcha_time = matcha_meta.modified().unwrap();
+    let matcha_result = std::fs::metadata(matcha_path).and_then(|data| data.modified());
+
+    // If we fail to get the modified time for any reason than assume we have to update
+    let Ok(matcha_time) = matcha_result else {
+        return true;
+    };
 
     let gleam_path = matcha_path.with_extension("gleam");
-    let gleam_meta = std::fs::metadata(gleam_path).unwrap();
-    let gleam_time = gleam_meta.modified().unwrap();
+    let gleam_result = std::fs::metadata(gleam_path).and_then(|data| data.modified());
 
-    if let Ok(_duration) = matcha_time.duration_since(gleam_time) {
-        true
-    } else {
-        false
-    }
+    // If we fail to get the modified time for any reason than assume we have to update
+    let Ok(gleam_time) = gleam_result else {
+        return true;
+    };
+
+    // Should update the gleam file if the matcha file is newer than it
+    matcha_time > gleam_time
 }
 
 #[derive(Debug, StructOpt)]
